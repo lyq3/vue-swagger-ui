3fac069a052b620ac9fb15178c27739d14f6dd40
罗云强 <99190092@qq.com>
2017-11-27 15:57:34 +0800

    vuex

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
+import Vuex from 'vuex'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/font-icon/iconfont.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
