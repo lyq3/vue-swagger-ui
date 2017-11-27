66f86d38e9f9be90aaec803d079f72d4f1bf8ade
罗云强 <99190092@qq.com>
2017-11-27 23:10:46 +0800

    color

--- src/components/apitest/ApiPanel.vue
+++ src/components/apitest/ApiPanel.vue
@@ -3,8 +3,9 @@
   <el-collapse-item name="1">
       <template slot="title" >
           <div style=" backgroundColor: gold; ">
-      一致性 Consistency<i class="header-icon el-icon-info"></i>
-      </div>
+                    请求参数
+                <i class="header-icon el-icon-info"></i>
+         </div>
     </template>
     <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
     <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
