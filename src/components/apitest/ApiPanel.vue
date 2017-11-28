edccdb8c57a11acab52f54e72602e72e5a9448aa
罗云强 <99190092@qq.com>
2017-11-28 22:40:03 +0800

    APiTest页面调整

--- src/components/apitest/ApiPanel.vue
+++ src/components/apitest/ApiPanel.vue
@@ -2,7 +2,7 @@
   <el-collapse v-model="activeNames" >
   <el-collapse-item name="1">
       <template slot="title" >
-          <div style=" backgroundColor: gold; ">
+          <div class="panel-title">
                     请求头
                 <i class="header-icon el-icon-info"></i>
          </div>
@@ -10,7 +10,13 @@
     <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
     <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
   </el-collapse-item>
-  <el-collapse-item title="请求参数" name="2">
+  <el-collapse-item  name="2">
+     <template slot="title" >
+          <div class="panel-title">
+                    请求参数
+                <i class="header-icon el-icon-info"></i>
+         </div>
+    </template>
     <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
     <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
   </el-collapse-item>
@@ -29,5 +35,17 @@ export default {
 </script>
 
 <style scoped>
-    
+    .panel-title {
+      background-color: #F0F9EB;
+      /* color: #fff; */
+      padding-left: 10px;
+      height: 50px;
+      line-height: 50px;
+      border-bottom: 1px solid #E5E5E5;
+      border-left: 1px solid #E5E5E5;
+      border-right: 1px solid #E5E5E5;
+      font-weight: 30px;
+      font-size: 15px;
+      color: #4CAF50;
+    }
 </style>
