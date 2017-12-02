7f2baa014d44ac7180e4e7a2271fe7d47f61ed39
罗云强 <99190092@qq.com>
2017-12-02 17:16:53 +0800

    index

--- src/components/NvaLeft.vue
+++ src/components/NvaLeft.vue
@@ -2,23 +2,19 @@
 <div class="leftBar" :style = "{width : changeLeftNavStyle}">
     <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="changeLeftNav"  text-color="#fff"
       active-text-color="#409EFF" background-color="#324157" >
-    <el-submenu index="1" v-for="(item, index) in getAPI">
+    <el-submenu :index="index" v-for="(item, index) in getAPI">
         <template slot="title">
         <i class="el-icon-location"></i>
         <span slot="title">{{item.name}}</span>
         </template>
-        <el-menu-item-group>
-        <span slot="title">分组一</span>
-        <el-menu-item index="1-1" >选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-        <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
+        <span v-for="(item2, index2) in item.paths">
+        <el-menu-item :index="index2 +''+ index">{{item2.summary}}</el-menu-item>
+        </span>
+        <!-- </el-menu-item-group>
         <el-submenu index="1-4">
         <span slot="title">选项4</span>
         <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
+        </el-submenu> -->
     </el-submenu>
     
     <el-menu-item index="2">
