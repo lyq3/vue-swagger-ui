c87c79474d1267e08a1c08a567c3ad3bf148bfeb
罗云强 <99190092@qq.com>
2017-12-06 23:05:51 +0800

    滚动条颜色

--- src/components/apitest/ApiDetails.vue
+++ src/components/apitest/ApiDetails.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class = "outerClass" v-bind:style="{ height: this.$store.state.windowHeight -100+'px'}">
       
       <el-collapse v-model="activeNames" >
         <el-collapse-item name="1" class="collapse-class"> 
@@ -9,38 +9,37 @@
                         <i class="header-icon el-icon-info"></i>
                 </div>
             </template>
-            <div>
-                <table class = "table-class">
+            <!--接口详情列表-->
+                <table class = "table-class des-table-class">
                     <tr>
                         <td style="width:200px">
                             URL
                         </td>
                         <td style="color:red">
-                            {{this.$store.getters.getMenuTreeObj[this.$route.params.start].paths[this.$route.params.end].path}}
+                            {{tree.path}}
                         </td>
                     </tr>
                     <tr>
                         <td >请求方式</td>
-                        <td style="color:#409EFF"> {{this.$store.getters.getMenuTreeObj[this.$route.params.start].paths[this.$route.params.end].sendWay.toUpperCase()}}</td>
+                        <td style="color:#409EFF"> {{tree.sendWay.toUpperCase()}}</td>
                     </tr>
                     <tr>
                         <td>接口名称</td>
-                        <td> {{this.$store.getters.getMenuTreeObj[this.$route.params.start].paths[this.$route.params.end].summary}}</td>
+                        <td> {{tree.summary}}</td>
                     </tr>
                     <tr>
                         <td>接口说明</td>
-                        <td> {{this.$store.getters.getMenuTreeObj[this.$route.params.start].paths[this.$route.params.end].description}}</td>
+                        <td> {{tree.description}}</td>
                     </tr>
                     <tr>
                         <td>consumes</td>
-                        <td> {{this.$store.getters.getMenuTreeObj[this.$route.params.start].paths[this.$route.params.end].consumes}}</td>
+                        <td> {{tree.consumes}}</td>
                     </tr>
                     <tr>
                         <td>produces</td>
-                        <td> {{this.$store.getters.getMenuTreeObj[this.$route.params.start].paths[this.$route.params.end].produces}}</td>
+                        <td> {{tree.produces}}</td>
                     </tr>
                 </table>
-            </div>
            
         </el-collapse-item>
         <el-collapse-item  name="2" class="collapse-class">
@@ -50,34 +49,61 @@
                         <i class="header-icon el-icon-info"></i>
                 </div>
             </template>
+            <!--接口参数列表-->
+                     <table class = "table-class parm-table-class">
+                    <tr>
+                        <td>参数名称</td>
+                        <td >描述</td>
+                        <td>类型</td>
+                        <td>IN</td>
+                        <td>是否必须</td>
+                    </tr>
+                    <tr v-for="key in tree.parameters">
+                        <td >{{key.name}}</td>
+                        <td >{{key.description}}</td>
+                        <td>{{key.type}}</td>
+                        <td>{{key.in}}</td>
+                        <td>{{key.required}}</td>
+                        
+                    </tr>
+                    
+                </table>
+        </el-collapse-item>
+         <el-collapse-item  name="2" class="collapse-class">
+            <template slot="title" >
+                <div class="panel-title">
+                            响应模型
+                        <i class="header-icon el-icon-info"></i>
+                </div>
+            </template>
                      <table class = "table-class">
                     <tr>
                         <td style="width:200px">
                             URL
                         </td>
                         <td style="color:red">
-                            {{this.$store.getters.getMenuTreeObj[this.$route.params.start].paths[this.$route.params.end].path}}
+                            {{tree.path}}
                         </td>
                     </tr>
                     <tr>
                         <td >请求方式</td>
-                        <td style="color:#409EFF"> {{this.$store.getters.getMenuTreeObj[this.$route.params.start].paths[this.$route.params.end].sendWay.toUpperCase()}}</td>
+                        <td style="color:#409EFF"> {{tree.sendWay.toUpperCase()}}</td>
                     </tr>
                     <tr>
                         <td>接口名称</td>
-                        <td> {{this.$store.getters.getMenuTreeObj[this.$route.params.start].paths[this.$route.params.end].summary}}</td>
+                        <td> {{tree.summary}}</td>
                     </tr>
                     <tr>
                         <td>接口说明</td>
-                        <td> {{this.$store.getters.getMenuTreeObj[this.$route.params.start].paths[this.$route.params.end].description}}</td>
+                        <td> {{tree.description}}</td>
                     </tr>
                     <tr>
                         <td>consumes</td>
-                        <td> {{this.$store.getters.getMenuTreeObj[this.$route.params.start].paths[this.$route.params.end].consumes}}</td>
+                        <td> {{tree.consumes}}</td>
                     </tr>
                     <tr>
                         <td>produces</td>
-                        <td> {{this.$store.getters.getMenuTreeObj[this.$route.params.start].paths[this.$route.params.end].produces}}</td>
+                        <td> {{tree.produces}}</td>
                     </tr>
                 </table>
         </el-collapse-item>
@@ -94,11 +120,23 @@ export default {
       };
     },
     computed: {
+        tree (){
+            return this.$store.getters.getMenuTreeObj[this.$route.params.start].paths[this.$route.params.end];
+        }
     }
 }
 </script>
 
 <style scoped>
+::-webkit-scrollbar{width:8px;}
+::-webkit-scrollbar-track{background-color:#fff;}
+::-webkit-scrollbar-thumb{background-color:#67C23A;}
+::-webkit-scrollbar-thumb:hover {background-color:#00aff0}
+::-webkit-scrollbar-thumb:active {background-color:#67C23A}
+    .outerClass {
+        overflow: auto;
+        overflow-x: hidden
+    }
     .panel-title {
       /* background-color: #F0F9EB; */
       /* color: #fff; */
@@ -133,7 +171,10 @@ export default {
     font-weight: bold;
    
     }
-    tr :first-child {
+    .des-table-class tr :first-child {
+         color: #868B9B;
+    }
+    .parm-table-class :first-child td{
          color: #868B9B;
     }
 </style>
