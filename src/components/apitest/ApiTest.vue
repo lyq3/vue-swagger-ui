0a6f1b81d47868fab3630010fbcfa66fc577c9e6
罗云强 <99190092@qq.com>
2017-12-07 23:31:29 +0800

    1

--- src/components/apitest/ApiTest.vue
+++ src/components/apitest/ApiTest.vue
@@ -2,7 +2,7 @@
   <div class = "outerClass" v-bind:style="{ height: this.$store.state.windowHeight -100+'px'}">
       <el-button type="primary" class="btn-class btn-float">{{tree.sendWay.toUpperCase()}}</el-button>
       <el-input  v-model="tree.path" placeholder="请输入内容" class="btn-class input-float" ></el-input>
-      <el-button type="success" class="btn-class">发送请求</el-button>
+      <el-button type="success" class="btn-class" @click="getSendParam">发送请求</el-button>
       <el-collapse v-model="activeName" >
       <!--参数面板-->
       <el-collapse-item  name="1" class="collapse-class">
@@ -15,17 +15,17 @@
             <!--接口参数列表-->
                      <table class = "table-class parm-table-class">
                     <tr>
-                        <td style="width:10px"><el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange"></el-checkbox></td>
+                        <td style="width:10px"><el-checkbox checked="checkAll"  v-model="checkAll"  @change="handleCheckAllChange"></el-checkbox></td>
                         <td style="width:200px">参数名称</td>
                         <td>参数值</td>
                         <td style="width:30px">删除</td>
                     </tr>
                     
-                    <tr v-for="key in tree.parameters">
-                        <td > <el-checkbox></el-checkbox></td>
+                    <tr v-for="(key,index) in tree.parameters">
+                        <td > <el-checkbox checked="true" v-model="key.checked"></el-checkbox></td>
                         <td ><el-input v-model="key.name"></el-input></td>
-                        <td><el-input ></el-input></td>
-                        <td><el-button size="small" type="danger">删除</el-button></td>
+                        <td><el-input :placeholder="key.description" v-model="key.value"></el-input></td>
+                        <td><el-button size="small" type="danger" @click="delParam(index)">删除</el-button></td>
                         
                     </tr>
                     
@@ -39,8 +39,16 @@
                         <i class="header-icon el-icon-upload"></i>
                 </div>
             </template>
+             <el-button style="margin:0 20px" size="small" @click="famtJSON" type="success" plain>格式化JSON</el-button>
+                    <el-switch
+                    v-model="openTree"
+                    active-text="展开所有节点">
+                    </el-switch>
+                    <pre id= "json-renderer_res" >
+                         {{res}}
+                     </pre>
             <!--接口参数列表-->
-                     <table class = "table-class parm-table-class">
+                     <!-- <table class = "table-class parm-table-class">
                     <tr>
                         <td style="width:100px"><el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox></td>
                         <td >参数名称</td>
@@ -56,7 +64,7 @@
                         
                     </tr>
                     
-                </table>
+                </table> -->
                  <div style="color:red; text-align: center" v-if="!tree.parameters">无参数</div>
         </el-collapse-item>
       </el-collapse>
@@ -69,7 +77,63 @@ export default {
   data(){
       return {
           url:tree.path,
-          activeName: ['1']
+          activeName: ['1'],
+          checkAll : true,
+          openTree :false,
+          marksTree : true,
+          res:'暂无数据'
+      }
+  },
+  methods : {
+      delParam(index){
+          this.tree.parameters.splice(index,1);
+      },
+      getSendParam(){
+          console.log(this.tree.parameters);
+          let _this = this;
+          let url = this.tree.path;
+          let loading ;
+          $.ajax({
+              url : '/webchat'+url,
+              data : {},
+              type : _this.tree.sendWay.toLowerCase(),
+              dataType : 'json',
+              beforeSend:function(){
+                loading =  _this.openFullScreen();
+              },
+              error:function(){
+                loading.close();
+              },
+              success : function(res){
+                loading.close();
+                _this.res = res;
+            
+              }
+          });
+      },
+      famtJSON(){
+          let _this = this;
+           $('#json-renderer_res').jsonViewer (eval(_this.res),{collapsed :!_this.openTree,withQuotes :!_this.marksTree});
+      },
+      handleCheckAllChange(){
+          let _this = this;
+          console.log(this.checkAll)
+          for(let i =0 ; i<_this.tree.parameters.length;i++ ){
+            if(_this.checkAll){
+                _this.tree.parameters[i].checked = true;
+            } else {
+                _this.tree.parameters[i].checked = false;
+            }
+          }
+      },
+      openFullScreen() {
+        const loading = this.$loading({
+          lock: true,
+          text: 'Loading',
+        //   spinner: 'el-icon-loading',
+        //   background: 'rgba(0, 0, 0, 0.7)'
+        });
+        return loading;
       }
   },
   components : {
