5c8f131148c6a50db53696085e62b91e08a1bfdf
lyq3 <99190092@qq.com>
2017-12-08 14:56:23 +0800

    更改打包文件名

--- config/index.js
+++ config/index.js
@@ -58,7 +58,7 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dist/vue-doc.html'),
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
